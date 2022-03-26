9ccc926abd51e1cba6cc49bae244355b6995ad94
Alex C <cxz980314@gmail.com>
2022-03-25 22:03:48 -0400

    merge skill page to about page

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -3,7 +3,6 @@ import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from "./components/Home";
 import About from './components/About';
-import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Footer from './components/Footer';
 import Contact from './components/Contact';
@@ -18,7 +17,6 @@ function App() {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/about" component={About} />
-          <Route exact path="/skills" component={Skills} />
           <Route exact path="/projects" component={Projects} />
           <Route exact path="/contact" component={Contact} />
         </Switch>
