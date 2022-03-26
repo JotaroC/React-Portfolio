0b86abe87ec0a6588728b655333cb9c69c790c63
Alex C <cxz980314@gmail.com>
2022-03-25 22:07:05 -0400

    fix hashhistory

--- react-portfolio/src/index.js
+++ react-portfolio/src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <BrowserRouter basename="/react-portfolio">
+        <App />
+    </BrowserRouter>,
+    document.getElementById("root")
 );
 
